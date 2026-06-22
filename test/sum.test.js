b12cb664786e42fe10aef205a3741f3551f6cdbc
yestin-claw <yestin-claw@local>
2026-06-23 07:18:44 +0800

    feat: add subtract for auto-merge demo

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,8 +1,9 @@
 const assert = require('assert');
-const { sum } = require('../src/index.js');
+const { sum, multiply, subtract } = require('../src/index.js');
 
-assert.strictEqual(sum(1, 2), 3, '1 + 2 should be 3');
-assert.strictEqual(sum(-1, 1), 0, '-1 + 1 should be 0');
-assert.strictEqual(sum(0, 0), 0, '0 + 0 should be 0');
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(multiply(2, 3), 6);
+assert.strictEqual(subtract(10, 4), 6);
+assert.strictEqual(subtract(0, 5), -5);
 
 console.log('✅ all tests passed');
